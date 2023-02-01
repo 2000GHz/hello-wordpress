c275c6573a65c97d5a1b2d20b0990ef4374ba9e0
Eloy Gracia <eloygb2000@gmail.com>
2023-02-01 12:29:17 +0100

    Minor changes

--- start-db.sh
+++ start-db.sh
@@ -6,7 +6,7 @@ docker run \
  --env MARIADB_USER=example-user \
  --env MARIADB_PASSWORD=my_cool_secret \
  --env MARIADB_ROOT_PASSWORD=sinensia \
- --env MARIADB_DATABASE=Wordpress \
+ --env MARIADB_DATABASE=wordpress \
  -p 3306:3306 \
  -v `pwd`/mariadb-data:/var/lib/mysql \
   mariadb:latest
